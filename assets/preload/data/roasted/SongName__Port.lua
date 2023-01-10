3da7400846988c51bbaff52ae10dda3ae9896c0a
BieuGuys <114687628+BieuGuys@users.noreply.github.com>
2023-01-10 11:41:58 -0300

    Add files via upload

---
+++ assets/preload/data/roasted/SongName__Port.lua
@@ -0,0 +1,5 @@
+function onCreate()
+makeLuaText('watermark', songName .. " | Cover by Hammerhead ", 0, 450, 50); -- You can edit the created by Prisma Text just dont change anything else
+    setTextSize('watermark', 15);
+    addLuaText('watermark');
+end
