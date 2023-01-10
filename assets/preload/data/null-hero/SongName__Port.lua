ea30d78d42195689356f136584c08bc4bd73ea17
BieuGuys <114687628+BieuGuys@users.noreply.github.com>
2023-01-10 11:32:33 -0300

    Add files via upload

---
+++ assets/preload/data/null-hero/SongName__Port.lua
@@ -0,0 +1,5 @@
+function onCreate()
+makeLuaText('watermark', songName .. " | OG by VoidNovaa, remix by Springman ", 0, 420, 50); -- You can edit the created by Prisma Text just dont change anything else
+    setTextSize('watermark', 15);
+    addLuaText('watermark');
+end
